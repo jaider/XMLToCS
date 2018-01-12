e216f1df720de300f8613acf950867007adfc233
Jaider <jaider.net@gmail.com>
2018-01-11 17:24:55 -0800

    Init Commit
    Add the solution with the class library required to convert XML into CSharp Code.

---
+++ XMLToCS.Lib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XMLToCS.Lib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jaider Ariza")]
+[assembly: AssemblyProduct("XMLToCS.Lib")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f14dd724-5012-414c-9218-423f7a6e718a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ XMLToCS.Lib/XMLCodeConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Xml.Linq;
+
+namespace XMLToCS.Lib
+{
+    public class XMLCodeConverter
+    {
+        Func<int, string> Tab = n => "".PadLeft(n * 4);
+
+        public string LoadAndConvert(string uri)
+        {
+            XDocument root = XDocument.Load(uri);
+            return Convert(root);
+        }
+
+        public string ParseAndConvert(string xmlContent)
+        {
+            XDocument root = XDocument.Parse(xmlContent);
+            return Convert(root);
+        }
+
+        public string Convert(XDocument root)
+        {
+            var result = new StringBuilder();
+            Nav(root.Root, ref result);
+            result.AppendLine(";");
+            return result.ToString();
+        }
+
+        private void Nav(XElement node, ref StringBuilder sb, int indentation = 0)
+        {
+            sb.Append($"{Tab(indentation)}new XElement(\"{node.Name}\"");
+
+            if (node.HasAttributes) {
+                foreach (var attr in node.Attributes()) {
+                    sb.AppendLine($"{Tab(indentation + 1)},");
+                    sb.Append($"{Tab(indentation + 1)}new XAttribute(\"{attr.Name}\", \"{attr.Value}\")");
+                }
+            }
+
+            if (node.HasElements) {
+                foreach (var child in node.Elements()) {
+                    sb.AppendLine($"{Tab(indentation + 1)},");
+                    Nav(child, ref sb, indentation + 1);
+                }
+                sb.AppendLine($"{Tab(indentation)})");
+            }
+            else {
+                if (!string.IsNullOrEmpty(node.Value)) {
+                    sb.Append($"{Tab(indentation + 1)}, \"{node.Value}\")");
+                }
+                else {
+                    sb.Append($"{Tab(indentation + 1)})");
+                }
+            }
+        }
+
+    }
+}
